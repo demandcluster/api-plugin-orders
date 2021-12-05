42b6b368bf18a5b6d52ffb00e72aa0cc602c38e0
tedraykov <tedraykov@gmail.com>
2021-12-05 14:00:12 +0200

    feat: after-tax pricing model support
    Signed-off-by: tedraykov <tedraykov@gmail.com>

--- src/util/addInvoiceToGroup.js
+++ src/util/addInvoiceToGroup.js
@@ -1,6 +1,33 @@
 import accounting from "accounting-js";
 
 /**
+ * @summary The taxes support two pricing modes: pre-tax after-tax pricing.
+ * When pre-tax is enabled, all defined prices of products, shipment and surcharges the tax is calculated onto do not
+ * include the tax.
+ *
+ * Pre-tax Pricing Example: Product costs 100$ and shipping is 5$. The total price not including tax is 105$.
+ * An example tax of 20% is calculated for the 105$ and the total becomes 105$ + 20% * 105$ = 126$
+ *
+ * After-tax Pricing Example: Product costs 100$ and shipping is 5$. When a tax of 20% is used for the shop with the
+ * after-tax pricing model this means the 20% tax is already included in the 105$ total price. In this case the total
+ * tax should not be added to the combined total.
+ * @param {Object[]} taxes - List of all applied taxes for the order
+ * @returns {number} tax total for all pre-tax pricing taxes.
+ */
+function calculatePreTaxPricingTaxTotal(taxes) {
+  let preTaxPricingTaxTotal = 0;
+
+  if (!taxes) return 0;
+
+  for (const { tax, customFields } of taxes) {
+    if (tax === null) return 0;
+    if (!customFields || !customFields.afterTaxPricing) preTaxPricingTaxTotal += tax;
+  }
+
+  return preTaxPricingTaxTotal;
+}
+
+/**
  * @summary Calculate final shipping, discounts, surcharges, and taxes; builds an invoice object
  *   with the totals on it; and sets group.invoice.
  * @param {String} currencyCode Currency code of totals
@@ -22,9 +49,14 @@ export default function addInvoiceToGroup({
   // Items
   const itemTotal = +accounting.toFixed(group.items.reduce((sum, item) => (sum + item.subtotal), 0), 3);
 
+  let taxes;
+  if (group.taxSummary) {
+    ({ taxes } = group.taxSummary);
+  }
+
   // Taxes
   const effectiveTaxRate = taxableAmount > 0 ? taxTotal / taxableAmount : 0;
-  const preTaxPricingTaxTotal = taxTotal + (taxableAmount - itemTotal);
+  const preTaxPricingTaxTotal = calculatePreTaxPricingTaxTotal(taxes);
 
   // Fulfillment
   const shippingTotal = group.shipmentMethod.rate || 0;
