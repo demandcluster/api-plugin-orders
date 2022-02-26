5a43188ee9b86842a9cb699085529b331d69e1bb
tedraykov <tedraykov@gmail.com>
2022-02-27 00:01:08 +0200

    feat: imageURLs in order query
    Signed-off-by: tedraykov <tedraykov@gmail.com>

--- src/resolvers/OrderItem/index.js
+++ src/resolvers/OrderItem/index.js
@@ -1,10 +1,12 @@
 import resolveShopFromShopId from "@reactioncommerce/api-utils/graphql/resolveShopFromShopId.js";
 import { encodeOrderItemOpaqueId } from "../../xforms/id.js";
+import imageURLs from "../../util/imageURLs.js";
 import productTags from "./productTags.js";
 
 export default {
   _id: (node) => encodeOrderItemOpaqueId(node._id),
   productTags,
   shop: resolveShopFromShopId,
-  status: (node) => node.workflow.status
+  status: (node) => node.workflow.status,
+  imageURLs: (node, args, context) => imageURLs(node, context)
 };
